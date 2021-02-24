1363c7a2ab9f2888aa869f7bc5d42f5c447d9e76
Ben Balls <bballs@spartaglobal.com>
2021-02-24 10:55:51 +0000

    minor changes

--- src/main/java/com/sparta/ben/App.java
+++ src/main/java/com/sparta/ben/App.java
@@ -1,21 +1,8 @@
 package com.sparta.ben;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main(String[] args) {
-        //CSVReader.readValues("src/main/resources/EmployeeRecords.csv");
-
-        //Starter.employeesDBStarter();
-
-        //Starter.CSVReaderTester();
-
-        //Starter.duplicateTester();
-
-        //Starter.DAOTester();
 
         MainProcess.start();
         System.out.println();
